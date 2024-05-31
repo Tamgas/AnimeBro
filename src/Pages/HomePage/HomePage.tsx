8faebb7086fa64952634446af03683df56cb4ed6
darknes7sjdhfouef <eshimbekovabdutahim@gmail.com>
2024-05-31 17:22:42 +0600

    added loading and anime genres. forgot to open the thread

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react';
+import { Title } from '../../Types/anime.types';
+import { $api } from '../../api';
+
 export const HomePage = () => {
+  const [title, setTitle] = useState<Title>();
+
+  useEffect(() => {
+    $api.get<Title>('/title/random')
+      .then(response => setTitle(response?.data));
+  }, []);
+  
+  console.log(title);
+  
+
   return (
     <>
+      <div className="container py-5 "></div>
     </>
   );
 };
